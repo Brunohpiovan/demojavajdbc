82099a5da141943c8cbbc98b45c57876dde98fb4
Bruno Piovan <bpiovan90@gmail.com>
2024-09-23 20:09:58 -0300

    Reutilizando a instanciacao

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,7 @@ public class Program {
 		
 		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
+		System.out.println("===== TEST 1 : seller findbyId=====");
 		Seller seller = sellerdao.findBYId(3);
 		
 		System.out.println(seller);
