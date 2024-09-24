0e6385f7e7df22a4303154bef87edd708163c4b7
Bruno Piovan <bpiovan90@gmail.com>
2024-09-24 12:22:03 -0300

    Implementacao de updates de dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,12 +32,19 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-	
 		System.out.println("\n===== TEST 4 : seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4002.5, department);
 		sellerdao.insert(newSeller);
 		System.out.println("Inserido! Novo id = "+newSeller.getId());
 		
+		System.out.println("\n===== TEST 5 : seller update =====");
+		seller = sellerdao.findBYId(1);
+		seller.setName("Paula Tejano");
+		sellerdao.update(seller);
+		System.out.println("Update completed");
+	
+	
 	}	
+	
 
 }
