7eac94970e629a7d0fa779db24812d628f0909e9
Bruno Piovan <bpiovan90@gmail.com>
2024-09-24 11:36:29 -0300

    implementação de inserçao de dados na DB

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,6 +31,13 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
-	}
+		
+	
+		System.out.println("\n===== TEST 4 : seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4002.5, department);
+		sellerdao.insert(newSeller);
+		System.out.println("Inserido! Novo id = "+newSeller.getId());
+		
+	}	
 
 }
